b12fb90a2c9f60cfd079eb6ac45558762d4f19a6
ensuta <ensuta08130813@gmail.com>
2023-10-22 15:51:58 +0900

    commit

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
-const https = require('https');
 const fs = require('fs');
 const path = require('path');
 
